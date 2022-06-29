aa1c6ae2bd147441c72cf209f9810d2946a0baff
Jorge Aguilar <jorge.agvel@gmail.com>
2022-06-28 23:43:24 -0500

    Add components logo and navbar

---
+++ components/logo.js
@@ -0,0 +1,42 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { Text, useColorModeValue, Box } from '@chakra-ui/react'
+import styled from '@emotion/styled'
+
+const LogoBox = styled.span`
+font-weight: bold;
+font-size: 18px;
+display: inline-flex;
+align-items: center;
+height: 30px;
+line-height: 20px;
+padding: 10px;
+
+&:hover {
+    transform rotate(20deg);
+}
+`
+
+const Logo = () => {
+  const footPrintImg = `/images/jletter${useColorModeValue('', '-dark')}.png`
+
+  return (
+    <Link href="/">
+      <a>
+        <LogoBox>
+          <Image src={footPrintImg} width={30} height={30} alt="logo" />
+          <Text
+            color={useColorModeValue('gray.800', 'whiteAlpha.900')}
+            fontFamily="M PLUS Rounded 1c"
+            fontWeight="bold"
+            ml={3}
+          >
+            Jorge Aguilar
+          </Text>
+        </LogoBox>
+      </a>
+    </Link>
+  )
+}
+
+export default Logo
