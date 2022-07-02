19dde8fa4c751fcb9b7ff54fed1a998b8223abf0
Jorge Aguilar <jorge.agvel@gmail.com>
2022-07-02 17:48:08 -0500

    Animate the toggle theme button

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import { IconButton, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import { SunIcon, MoonIcon } from '@chakra-ui/icons'
 
@@ -5,12 +6,22 @@ const ThemeToggleButton = () => {
   const { toggleColorMode } = useColorMode()
 
   return (
-    <IconButton
-      aria-label="Toggle theme"
-      colorScheme={useColorModeValue('purple', 'yellow')}
-      icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
-      onClick={toggleColorMode}
-    ></IconButton>
+    <AnimatePresence exitBeforeEnter inital={false}>
+      <motion.div style = {{ display: "inline-block"}}
+      key = {useColorModeValue('light', 'dark')}
+      initial = {{ opacity: 0, y: -20 }}
+      animate = {{ opacity: 1, y: 0 }}
+      exit = {{y: 20, opacity:0}}
+      transition = {{ duration: 0.2 }}
+      >
+        <IconButton
+          aria-label="Toggle theme"
+          colorScheme={useColorModeValue('purple', 'yellow')}
+          icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+          onClick={toggleColorMode}
+        ></IconButton>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
