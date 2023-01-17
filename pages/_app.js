112223fff5c22c0551d1a103836fd13fe8f5b6bf
Jorge Aguilar <jorge.aguilar@gluo.mx>
2023-01-16 19:52:40 -0600

    Fix GA

--- pages/_app.js
+++ pages/_app.js
@@ -14,13 +14,18 @@ const Website = ({ Component, pageprops, router }) => {
           <Component {...pageprops} key={router.route} />
         </AnimatePresence>
       </Layout>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-12S93LP2TY"></Script>
-      <Script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments)}
-        gtag('js', new Date());
-
-        gtag('config', 'G-12S93LP2TY');
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-12S93LP2TY"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+          
+          gtag('config', 'G-12S93LP2TY');
+        `}
       </Script>
     </ChakraProvider>
   )
